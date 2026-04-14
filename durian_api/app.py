3dac9dd6652ad09206d5480e0a577f1cf42dd1af
unknown <michael22si@mahasiswa.pcr.ac.id>
2026-04-14 20:05:05 +0700

    ff a4

--- durian_api/app.py
+++ durian_api/app.py
@@ -6,6 +6,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
 
+GDRIVE_FOLDER_ID = os.environ.get('GDRIVE_FOLDER_ID', '')
+
 app = Flask(__name__)
 CORS(app)
 
